6ad575292c46ea28a70e467509a4326cc9c2d377
Luca Bruno <luca.bru@protonmail.com>
2021-10-25 10:47:38 +0200

    Some Kaggle esercitation

---
+++ Kaggle/001_house_pricing/housePricing.py
@@ -0,0 +1,12 @@
+import pandas as ps
+
+path = "melb_data.csv"
+
+#store data in a specific dataframe
+melb_data = ps.read_csv(path)
+
+#print summary of the data
+print(melb_data.describe())
+
+
+print('porcodio')
